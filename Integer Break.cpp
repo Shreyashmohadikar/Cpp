825d2fad48afd6e84ca0daa4df3baf1a929cd775
Shreyash <shreyashmohadikar@gmail.com>
2023-07-25 20:11:22 +0530

    Integer Break

---
+++ Integer Break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int n, vector<int> &dp){
+        if(n==0){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int maxpro = n;
+        for(int i=1; i<=n/2; i++){
+            maxpro = max(maxpro, i * f(n-i, dp));
+        }
+        return dp[n] = maxpro;
+    }
+    int integerBreak(int n) {
+        if(n<4){
+            return n-1;
+        }
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
+    }
+};
