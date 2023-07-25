710d9288d8c361e920c842eef7513e30410ed25c
Shreyash <shreyashmohadikar@gmail.com>
2023-07-25 19:43:41 +0530

    n

--- Rod cutting problem.cpp
+++ Rod cutting problem.cpp
@@ -8,14 +8,14 @@ int f(int idx, int N, vector<int> &price, vector<vector<int>> &dp){
 	}
 	int notpick = 0 + f(idx-1, N, price, dp);
 	int pick = INT_MIN;
-	int rodLength = idx + 1; 
+	int rodLength = idx + 1;
 	if(N >= rodLength){
 		pick = price[idx] + f(idx, N - rodLength, price, dp);
 	}
 	return dp[idx][N] = max(pick, notpick);
 }
-    
+
 int cutRod(vector<int> &price, int n) {
 	vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-	return f(n-1, n, price, dp); 
+	return f(n-1, n, price, dp);
 }
