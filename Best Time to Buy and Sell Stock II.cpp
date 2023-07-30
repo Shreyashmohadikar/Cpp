d0863bde3a3551d8fc4481acf0fafe31d47f85be
Shreyash <shreyashmohadikar@gmail.com>
2023-07-30 23:52:25 +0530

    new

--- Best Time to Buy and Sell Stock II.cpp
+++ Best Time to Buy and Sell Stock II.cpp
@@ -52,3 +52,29 @@ public:
         return f(0, true, prices, dp);
     }
 };
+
+//////////////////////
+//Tabulation
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        dp[n][0] = dp[n][1] = 0; // base case
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = INT_MIN;
+                if (buy) {
+                    profit = max(-prices[i] + dp[i + 1][1], 0 + dp[i + 1][0]);
+                } else {
+                    profit = max(prices[i] + dp[i + 1][1], 0 + dp[i + 1][0]);
+                }
+                dp[i][buy] = profit; // update the dp table
+            }
+        }
+        return dp[0][1]; // return the maximum profit achieved by selling on the first day
+    }
+};
