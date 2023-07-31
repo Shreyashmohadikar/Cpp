5d6bffe2617eb56f637bfc844ebe66c4d0de4add
Shreyash <shreyashmohadikar@gmail.com>
2023-07-31 10:45:36 +0530

    Tabulation

--- Best Time to Buy and Sell Stock III.cpp
+++ Best Time to Buy and Sell Stock III.cpp
@@ -25,3 +25,44 @@ public:
         return f(0, 1, 2, prices, dp);
     }
 };
+
+///////////////////////////////////////////////
+//tabulation
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, 
+                                    vector<vector<int>> 
+                                    (2,vector<int> (3, 0))); //3D DP
+
+        // if (n == 0) return 0;
+        //Base Case
+        // for(int i=n-1; i>=0; i--){
+        //     for(int buy=0; buy<=1; buy++){
+        //         dp[i][buy][0] = 0;
+        //     }
+        // }
+        // for(int i=n-1; i>=0; i--){
+        //     for(int buy=0; buy<=1; buy++){
+        //         dp[i][buy][0] = 0;
+        //     }
+        // }
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                for(int cap=1; cap<=2; cap++){
+                    int profit = INT_MIN;
+                    if(buy == 0){
+                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][1][cap],
+                                    0 + dp[i+1][0][cap]);
+                    }
+                    else{
+                        dp[i][buy][cap] = max(prices[i] + dp[i+1][0][cap-1],
+                                    0 + dp[i+1][1][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
+    }
+};
