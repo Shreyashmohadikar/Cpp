5a3fe39a9eea46f56a137c259fb420fcc4ba8c94
Shreyash <shreyashmohadikar@gmail.com>
2023-07-31 10:16:23 +0530

    Best Time to Buy and Sell Stock III

---
+++ Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int f(int i, int buy, int cap, vector<int> &prices, vector<vector<vector<int>>> &dp){
+        if(cap == 0 || i == prices.size()){
+            return 0;
+        }
+        if(dp[i][buy][cap] != -1){
+            return dp[i][buy][cap];
+        }
+        int profit = INT_MIN;
+        if(buy){
+            profit = max(-prices[i] + f(i+1, 0, cap, prices, dp) ,
+                        0 + f(i+1, 1, cap, prices, dp));
+        }
+        else{
+            profit = max(prices[i] + f(i+1, 1, cap-1, prices, dp),
+                        0 + f(i+1, 0, cap, prices, dp));
+        }
+        return dp[i][buy][cap] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size(), 
+                                        vector<vector<int>> (3, 
+                                            vector<int> (3, -1)));
+        return f(0, 2, 2, prices, dp);
+    }
+};
