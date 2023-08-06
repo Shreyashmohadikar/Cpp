efe6d386cbf896d02376c94b7183cb5208cebeed
Shreyash <shreyashmohadikar@gmail.com>
2023-08-06 22:04:53 +0530

    new

--- Longest Increasing SubsequenceLOOOOK.cpp
+++ Longest Increasing SubsequenceLOOOOK.cpp
@@ -43,4 +43,27 @@ public:
     }
 };
 
+////////////////////////////////////////////////
+//Binary Search
 
+#include <algorithm>
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int len = 1;
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1; i<n; i++){
+            if(temp.back() < nums[i]){
+                temp.push_back(nums[i]);
+                len++;
+            }
+            else{
+                int idx = lower_bound(temp.begin(), temp.end(), nums[i])- temp.begin();
+                temp[idx] = nums[i];
+            }
+        }
+        return temp.size();
+    }
+};
