385a06aac36fef51141f39cc9f0a4fffaf950551
Shreyash <shreyashmohadikar@gmail.com>
2023-08-09 16:44:28 +0530

    new

--- Longest Bitonic Sequence.cpp
+++ Longest Bitonic Sequence.cpp
@@ -2,53 +2,39 @@
 #include <vector>
 using namespace std;
 
-int f(int i, int prev, vector<int> &nums, vector<vector<int>> &dp){
-    if(i == nums.size()){
-        return 0;
-    }
-    if(dp[i][prev+1] != -1){
-        return dp[i][prev+1];
-    }
-    int notTake = f(i+1, prev, nums, dp);
-    int take = 0;
-    if(prev == -1 || nums[i] > nums[prev]){
-        take = 1 + f(i+1, i, nums, dp);
-    }
-    return dp[i][prev+1] = max(take, notTake);
-}
+int longestBitonicSubsequence(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> lis(n, 1); // Longest Increasing Subsequence ending at index i
+    vector<int> lds(n, 1); // Longest Decreasing Subsequence starting from index i
 
-int g(int i, int prev, vector<int> &nums, vector<vector<int>> &dp1){
-    if(i == nums.size()){
-        return 0;
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (arr[i] > arr[j]) {
+                lis[i] = max(lis[i], lis[j] + 1);
+            }
+        }
     }
-    if(dp1[i][prev+1] != -1){
-        return dp1[i][prev+1];
-    }
-    int notTake = g(i+1, prev, nums, dp1);
-    int take = 0;
-    if(prev == -1 || nums[i] < nums[prev]){
-        take = 1 + g(i+1, i, nums, dp1);
-    }
-    return dp1[i][prev+1] = max(take, notTake);
-}
 
-int longestBitonicSubsequence(vector<int>& arr, int n)
-{
-    vector<vector<int>> dp(n, vector<int>(n+1, -1));
-    vector<vector<int>> dp1(n, vector<int>(n+1, -1));
-    int left = f(0, -1, arr, dp);
-    int right = g(0, -1, arr, dp1);
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = n - 1; j > i; --j) {
+            if (arr[i] > arr[j]) {
+                lds[i] = max(lds[i], lds[j] + 1);
+            }
+        }
+    }
+
     int maxBitonicLength = 0;
     for (int i = 0; i < n; ++i) {
-        maxBitonicLength = max(maxBitonicLength, dp[i][i] + dp1[i][i] - 1);
+        maxBitonicLength = max(maxBitonicLength, lis[i] + lds[i] - 1);
     }
+
     return maxBitonicLength;
 }
 
 int main() {
-    vector<int> arr = {1, 3, 5, 3, 2};
-    int n = arr.size();
-    int result = longestBitonicSubsequence(arr, n);
+    vector<int> arr = {1, 3, 5, 3, 21, 1, 2, 3, 4};
+    int result = longestBitonicSubsequence(arr);
     cout << "Length of the longest bitonic subsequence: " << result << endl;
+
     return 0;
 }
