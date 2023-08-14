05cb1d9518e7881f7e4df5485b5d6b9e858955f4
Shreyash <shreyashmohadikar@gmail.com>
2023-08-15 00:38:00 +0530

    new

--- Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -1,22 +1,28 @@
-class Solution {
+class Solution
+{
 public:
     const int MOD = 1e9 + 7;
-    int numSubseq(vector<int>& nums, int target) {
+    int numSubseq(vector<int> &nums, int target)
+    {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> dp(n+1, 1);
-        for(int i=1; i<=n; i++){
-            dp[i] = (dp[i-1] * 2) % MOD;
+        vector<int> dp(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = (dp[i - 1] * 2) % MOD;
         }
 
-        int left=0, right=n-1;
+        int left = 0, right = n - 1;
         int ans = 0;
-        while(left <= right){
-            if(nums[left] + nums[right] <= target){
+        while (left <= right)
+        {
+            if (nums[left] + nums[right] <= target)
+            {
                 ans = (ans + dp[right - left]) % MOD;
                 left++;
             }
-            else{
+            else
+            {
                 right--;
             }
         }
@@ -24,6 +30,4 @@ public:
     }
 };
 
-
 // Number of Subsequences That Satisfy the Given Sum Condition
-
