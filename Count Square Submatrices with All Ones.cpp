7cb971ad53110da4fff9a74f8b0614486e92a445
Shreyash <shreyashmohadikar@gmail.com>
2023-08-19 21:06:03 +0530

    Count Square Submatrices with All Ones

--- Count Square Submatrices with All Ones.cpp
+++ Count Square Submatrices with All Ones.cpp
@@ -24,7 +24,7 @@ public:
                 }
             }
         }
-        int count=0;
+        int count = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 count += dp[i][j];
