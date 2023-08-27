df8ec6e432c3f79a966405b7741014ae36538e73
Shreyash <shreyashmohadikar@gmail.com>
2023-08-27 19:26:25 +0530

    Sqrt(x)

---
+++ Practice/Sqrt(x).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x <= 1 ){
+            return x;
+        }
+        int L = 0, R = x;
+        while(L <= R){
+            int mid = L + (R-L) / 2;
+            if(mid == x / mid){
+                return mid;
+            }
+            else if(mid > x / mid){
+                R = mid - 1;
+            }
+            else{
+                L = mid + 1;
+            }
+        }
+        return R;
+    }
+};
