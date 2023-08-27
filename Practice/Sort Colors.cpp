25869d52200b5a5183d5e394e290c4c7d2460c11
Shreyash <shreyashmohadikar@gmail.com>
2023-08-28 02:14:32 +0530

    Sort Colors

--- Practice/Sort Colors.cpp
+++ Practice/Sort Colors.cpp
@@ -13,3 +13,41 @@ public:
         return;
     }
 };
+
+
+/////
+
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int c0 = 0, c1 = 0, c2 = 0;
+        for(auto it : arr){
+            if(it == 0){
+                c0++;
+            }
+            if(it == 1){
+                c1++;
+            }
+            if(it == 2){
+                c2++;
+            }
+        }
+        int i = 0;
+        while(c0 != 0){
+            arr[i] = 0;
+            c0--;
+            i++;
+        }
+        while(c1 != 0){
+            arr[i] = 1;
+            c1--;
+            i++;
+        }
+        while(c2 != 0){
+            arr[i] = 2;
+            c2--;
+            i++;
+        }
+        return;
+    }
+};
