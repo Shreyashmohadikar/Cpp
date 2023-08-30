fdb7305f8b912a33b0494074ea188765c64e7e80
Shreyash <shreyashmohadikar@gmail.com>
2023-08-30 16:47:36 +0530

    Permutations

---
+++ Practice/Permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void swap(vector<int> &nums, int i, int idx){
+        int temp = nums[i];
+        nums[i] = nums[idx];
+        nums[idx] = temp;
+    }
+    void f(int idx, vector<int> &nums,
+            vector<vector<int>> &ans){
+
+        if(idx == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=idx; i<nums.size(); i++){
+            swap(nums, i, idx);
+            f(idx+1, nums, ans);
+            swap(nums, i, idx);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        if(nums.size()){
+            f(0, nums, ans);
+        }
+        return ans;
+    }
+};
