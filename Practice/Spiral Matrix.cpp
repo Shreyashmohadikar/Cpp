7a72844a7597e0a640458d74dbdcc72d04c2ec57
Shreyash <shreyashmohadikar@gmail.com>
2023-08-30 12:07:00 +0530

    Spiral Matrix

--- Practice/Spiral Matrix.cpp
+++ Practice/Spiral Matrix.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> ans;
+
         if(matrix.size() == 0 || matrix[0].size() == 0){
             return ans;
         }
+
         int rowStart = 0, colStart = 0;
         int rowEnd = matrix.size()-1, colEnd = matrix[0].size()-1;
+
         while(rowStart <= rowEnd && colStart <= colEnd){
             //Traverse Right
             for(int i=colStart; i<=colEnd; i++){
                 ans.push_back(matrix[rowStart][i]);
             }
             rowStart++;
+
             //Traverse Down
             for(int i=rowStart; i<=rowEnd; i++){
                 ans.push_back(matrix[i][colEnd]);
             }
             colEnd--;
+
             //Traverse Left
             if(rowStart <= rowEnd){
                 for(int i=colEnd; i>=colStart; i--){
@@ -25,6 +30,7 @@ public:
                 }
                 rowEnd--;
             }
+
             //Traverse Up
             if(colStart <= colEnd){
                 for(int i=rowEnd; i>=rowStart; i--){
@@ -32,7 +38,7 @@ public:
                 }
                 colStart++;
             }
-        }
+        } 
         return ans;
     }
 };
