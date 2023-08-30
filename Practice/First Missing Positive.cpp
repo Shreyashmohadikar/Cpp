62d9bc4f8de13d1a615ee3d8600733d1e117f3ee
Shreyash <shreyashmohadikar@gmail.com>
2023-08-31 02:10:56 +0530

    First Missing Positive

--- Practice/First Missing Positive.cpp
+++ Practice/First Missing Positive.cpp
@@ -12,3 +12,20 @@ public:
         return i;
     }
 };
+
+////////////////////
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i=1;
+        unordered_set<int> st;
+        for(int i=0; i<nums.size(); i++){
+            st.insert(nums[i]);
+        }
+        while(st.count(i)){
+            i++;
+        }
+        return i;
+    }
+};
