10414a999c637a5fcad0725a6da4b930dc2d17cc
Shreyash <shreyashmohadikar@gmail.com>
2023-09-06 02:03:43 +0530

    Cost

--- Practice/graph.cpp
+++ Practice/graph.cpp
@@ -2,25 +2,22 @@
 using namespace std;
 
 int main(){
-    vector<vector<int>> arr = {{0,1}, {1,2}, {3,4}, {2,4}, {2,6}, {5,4}};
+    int ** arr[][] = {{0,1}, {1,2}, {3,4}, {2,4}, {2,6}, {5,2}};;
     int N = 7;
     int M = 6;
     int K = 5;
-
     unordered_map<int, int> mp;
     for(int i=0; i<M; i++){
         mp[arr[i][0]]++;
         mp[arr[i][1]]++;
     }
-
     int count = 0;
-    for(int i=0; i<N; i++){
-        if(mp[i] > 1){
+    for(auto it : mp){
+        if(it.second > 1){
             count++;
         }
     }
-
     int ans = count * K;
-    cout<<endl<<"Cost: "<<ans<<endl;
+    cout<<"Cost: "<<ans;
     return 0;
 }
