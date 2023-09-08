5a7ddb632e363ee159f6308188273d7733eba688
Shreyash <shreyashmohadikar@gmail.com>
2023-09-08 15:12:59 +0530

    Pascal's Triangle

--- Practice/Pascal's Triangle.cpp
+++ Practice/Pascal's Triangle.cpp
@@ -13,4 +13,25 @@ public:
     }
 };
 
+//////////////////////
+//Using Recursion
 
+
+class Solution {
+public:
+    vector<vector<int>> pascal(int row, int numRows, vector<vector<int>> &ans){
+        if(row == numRows){
+            return ans;
+        }
+        vector<int> temp(row + 1, 1);
+        for(int j=1; j<row; j++){
+            temp[j] = ans[row-1][j-1] + ans[row-1][j]; 
+        }
+        ans.push_back(temp);
+        return pascal(row+1, numRows, ans);
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        return pascal(0, numRows, ans);
+    }
+};
