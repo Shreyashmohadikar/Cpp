94b5b96bdfc57b80c2e12504a65686f1db802783
Shreyash <shreyashmohadikar@gmail.com>
2023-09-11 19:02:23 +0530

    Group the People Given the Group Size They Belong

--- Practice/Group the People Given the Group Size They Belong To.cpp
+++ Practice/Group the People Given the Group Size They Belong To.cpp
@@ -1,17 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> groupThePeople(vector<int> &groupSizes)
-    {
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         vector<vector<int>> ans;
         unordered_map<int, vector<int>> mp;
 
-        for (int i = 0; i < groupSizes.size(); i++)
-        {
+        for(int i=0; i<groupSizes.size(); i++){
             int grpSize = groupSizes[i];
             mp[grpSize].push_back(i);
-            if (mp[grpSize].size() == grpSize)
-            {
+            if(mp[grpSize].size() == grpSize){
                 ans.push_back(mp[grpSize]);
                 mp[grpSize].clear();
             }
