d64543bb1fbc846940e7ccb83c9d1d2e4b36c166
Shreyash <shreyashmohadikar@gmail.com>
2023-09-13 17:17:49 +0530

    Candy

--- Practice/Candy LOOOK.cpp
+++ Practice/Candy LOOOK.cpp
@@ -1,20 +1,27 @@
-class Solution {
+class Solution
+{
 public:
-    int candy(vector<int>& ratings) {
+    int candy(vector<int> &ratings)
+    {
         int n = ratings.size();
         vector<int> candyArr(n, 1);
-        for(int i=1; i<n; i++){
-            if(ratings[i] > ratings[i-1]){
-                candyArr[i] = candyArr[i-1] + 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                candyArr[i] = candyArr[i - 1] + 1;
             }
         }
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                candyArr[i] = max(candyArr[i], candyArr[i+1] + 1);
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1])
+            {
+                candyArr[i] = max(candyArr[i], candyArr[i + 1] + 1);
             }
         }
         int totalCandy = 0;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++)
+        {
             totalCandy += candyArr[i];
         }
         return totalCandy;
