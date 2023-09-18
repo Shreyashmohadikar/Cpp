53edd1565dcda19dc0d61a8cd3f114b1b668e5c3
Shreyash <shreyashmohadikar@gmail.com>
2023-09-18 15:52:58 +0530

    The K Weakest Rows in a Matrix

--- Practice/The K Weakest Rows in a Matrix.cpp
+++ Practice/The K Weakest Rows in a Matrix.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        priority_queue<pair<int, int>, 
-                        vector<pair<int, int>>, 
+        priority_queue<pair<int, int>,
+                        vector<pair<int, int>>,
                         greater<pair<int, int>>> pq;
         for(int i=0; i<mat.size(); i++){
             int sum = 0;
