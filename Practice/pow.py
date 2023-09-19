06fbac2916f32a36f3f0c8f300c0dd38cf148cca
Shreyash <shreyashmohadikar@gmail.com>
2023-09-19 23:29:47 +0530

    pow

--- Practice/pow.py
+++ Practice/pow.py
@@ -1,19 +1,21 @@
-# def sumOfDigits(n):
-#     power = 2**n
-#     return power % 9
-
 def sumOfDigits(n):
     power = 2**n
-    while(power > 0):
-        sumDig += power % 10
-        power = power / 10
-    while(sumDig > 9):
-        sumDig = sum([int(digit) for digit int str(sumDig)])
-    return sumDig
+    return power % 9
+
+# def sumOfDigits(n):
+#     power = pow(2, n)
+#     sumDig = 0
+#     while power > 0:
+#         sumDig += power % 10
+#         power = power // 10
+#     while power > 9:
+#         sumDig = sum([int(digit) for digit in str(sumDig)])
+#     return sumDig
 
 t = int(input())
-while t>0:
+while t > 0:
     n = int(input())
     print(sumOfDigits(n))
     t -= 1
+
 
