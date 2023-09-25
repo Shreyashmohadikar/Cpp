999af63233dfdcae7a10f27088bc81d5770b9754
Shreyash <shreyashmohadikar@gmail.com>
2023-09-25 21:37:01 +0530

    .

--- Practice/Candy LOOOK.cpp
+++ Practice/Candy LOOOK.cpp
@@ -9,7 +9,6 @@ public:
                 candyArr[i] = candyArr[i-1] + 1;
             }
         }
-
         for(int i=n-2; i>=0; i--){
             if(ratings[i] > ratings[i+1]){
                 candyArr[i] = max(candyArr[i], candyArr[i+1] + 1);
