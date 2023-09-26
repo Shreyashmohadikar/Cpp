5998e3c0be15ed334cc019326d48e3f75c6a4eb7
Shreyash <shreyashmohadikar@gmail.com>
2023-09-26 15:26:36 +0530

    Remove Duplicate Letters

---
+++ Practice/Remove Duplicate Letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> freq(26);
+        unordered_map<char, int> vis(26);
+        string ans = "";
+        for(auto ch : s){
+            freq[ch]++;
+        }
+        for(auto ch : s){
+            freq[ch]--;
+            if(vis[ch]){
+                continue;
+            }
+            while(!ans.empty() && freq[ans.back()] && ans.back()>ch){
+                vis[ans.back()] = 0;
+                ans.pop_back();
+            }
+            ans.push_back(ch);
+            vis[ch] = 1;
+        }
+        return ans;
+    }
+};
