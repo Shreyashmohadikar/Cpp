3853ec35a3deb0651301be4c9d25d5b4821a46f7
Shreyash <shreyashmohadikar@gmail.com>
2023-09-28 15:55:23 +0530

    Sort Array By Parity

---
+++ Practice/Sort Array By Parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> arr;
+        for(int it : nums){
+            if(it % 2 == 0){
+                arr.push_back(it);
+            }
+        }
+        for(int it : nums){
+            if(it % 2 != 0){
+                arr.push_back(it);
+            }
+        }
+        return arr;
+    }
+};
