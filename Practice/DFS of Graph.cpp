2c01a5c9434f0630f2fe3f25059260a0db237f89
Shreyash <shreyashmohadikar@gmail.com>
2023-09-29 19:02:34 +0530

    DFS

--- Practice/DFS of Graph.cpp
+++ Practice/DFS of Graph.cpp
@@ -3,21 +3,27 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution {
-  private:
-    void dfs(int node, vector<int> adj[], int vis[], vector<int> &ans){
+class Solution
+{
+private:
+    void dfs(int node, vector<int> adj[], int vis[], vector<int> &ans)
+    {
         vis[node] = 1;
         ans.push_back(node);
 
-        for(auto it : adj[node]){
-            if(!vis[it]){
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
                 dfs(it, adj, vis, ans);
             }
         }
     }
-  public:
+
+public:
     // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> dfsOfGraph(int V, vector<int> adj[])
+    {
         vector<int> ans;
         int vis[V] = {0};
 
@@ -27,16 +33,19 @@ class Solution {
 };
 
 //{ Driver Code Starts.
-int main() {
+int main()
+{
     int tc;
     cin >> tc;
-    while (tc--) {
+    while (tc--)
+    {
         int V, E;
         cin >> V >> E;
 
         vector<int> adj[V];
 
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < E; i++)
+        {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
@@ -46,7 +55,8 @@ int main() {
         // cin>>s1;
         Solution obj;
         vector<int> ans = obj.dfsOfGraph(V, adj);
-        for (int i = 0; i < ans.size(); i++) {
+        for (int i = 0; i < ans.size(); i++)
+        {
             cout << ans[i] << " ";
         }
         cout << endl;
