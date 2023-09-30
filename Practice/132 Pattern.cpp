26bedf67a9cd14a9c7c9633647f631af6b5d4501
Shreyash <shreyashmohadikar@gmail.com>
2023-09-30 13:50:57 +0530

    132 Pattern

---
+++ Practice/132 Pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int> stk;
+        int third = INT_MIN;
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] < third){
+                return true;
+            }
+            while(!stk.empty() && stk.top() < nums[i]){
+                third = stk.top();
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        return false;
+    }
+};
+
+/////////////////////////
+
