059cd080197d000f2f1425a3a31538b6d3e354d8
Shreyash <shreyashmohadikar@gmail.com>
2023-10-14 15:59:27 +0530

    0 1

--- DP/0 1 Knapsack.cpp
+++ DP/0 1 Knapsack.cpp
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
-int f(vector<int> &val, vector<int> &weight, int idx, int w, 
-	vector<vector<int>> &dp){
-	if(idx == 0){
-		if(weight[0] <= w){
+int f(vector<int> &val, vector<int> &weight, int idx, int w,
+	  vector<vector<int>> &dp)
+{
+	if (idx == 0)
+	{
+		if (weight[0] <= w)
+		{
 			return val[0];
 		}
-		else{
+		else
+		{
 			return 0;
 		}
 	}
-	if(dp[idx][w] != -1){
+	if (dp[idx][w] != -1)
+	{
 		return dp[idx][w];
 	}
 	int notTake = f(val, weight, idx - 1, w, dp);
 	int take = INT_MIN;
-	if(w >= weight[idx]){
-		take =  val[idx] + f(val, weight, idx - 1, w-weight[idx], dp);
+	if (w >= weight[idx])
+	{
+		take = val[idx] + f(val, weight, idx - 1, w - weight[idx], dp);
 	}
 	return dp[idx][w] = max(take, notTake);
 }
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 {
-	vector<vector<int>> dp(n, vector<int>(maxWeight+1, -1));
-	return f(value, weight, n-1, maxWeight, dp);
+	vector<vector<int>> dp(n, vector<int>(maxWeight + 1, -1));
+	return f(value, weight, n - 1, maxWeight, dp);
 }
