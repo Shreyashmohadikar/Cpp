5fe6a1b31ecc5b16ecad6dd765c3d4d7f991968a
Shreyash <shreyashmohadikar@gmail.com>
2023-10-26 17:02:56 +0530

    Two Sum

---
+++ Practice/2Sum.cpp
@@ -0,0 +1,37 @@
+//Brute Force
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]+nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+//Optimized
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            int complement = target - nums[i];
+            if(mp.count(complement)){
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
