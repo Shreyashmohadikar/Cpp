3986dc5e1b373d2b1df765ab87fd5e8b898877e5
Shreyash <shreyashmohadikar@gmail.com>
2023-11-07 17:28:39 +0530

    Rotate Image

--- Practice/Rotate Image.cpp
+++ Practice/Rotate Image.cpp
@@ -1,14 +1,44 @@
-class Solution {
+class Solution
+{
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void rotate(vector<vector<int>> &matrix)
+    {
         int n = matrix.size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
                 swap(matrix[i][j], matrix[j][i]);
             }
-        }//transpose
-        for(int i=0; i<n; i++){
+        } // transpose
+        for (int i = 0; i < n; i++)
+        {
             reverse(matrix[i].begin(), matrix[i].end());
-        }//reverse columns
+        } // reverse columns
+    }
+};
+
+// for anti-clockwise rotation
+
+class Solution
+{
+public:
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int>> &matrix, int n)
+    {
+        // Reverse each row
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
     }
 };
