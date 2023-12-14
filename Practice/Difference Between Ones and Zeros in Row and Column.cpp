5c4260dc539c42f3da8dc8be08bb3c4729a66052
Shreyash <shreyashmohadikar@gmail.com>
2023-12-14 19:31:22 +0530

    Difference Between Ones and Zeros in Row and Column

---
+++ Practice/Difference Between Ones and Zeros in Row and Column.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+        unordered_map<int, int> onesRow, onesCol, zerosRow, zerosCol;
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    onesRow[i]++;
+                    onesCol[j]++;
+                }
+                else{
+                    zerosRow[i]++;
+                    zerosCol[j]++;
+                }
+            }
+        }
+        vector<vector<int>> diff(m, vector<int>(n, 0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                diff[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j];
+            }
+        }
+        return diff;
+    }
+};
