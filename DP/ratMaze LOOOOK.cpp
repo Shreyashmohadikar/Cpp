a5055a1fe3463f7cfa45be7019dfbf5626c6948c
Shreyash <shreyashmohadikar@gmail.com>
2023-12-19 14:28:21 +0530

    rat Maze

--- DP/ratMaze LOOOOK.cpp
+++ DP/ratMaze LOOOOK.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool solve(int i, int j, vector<vector<int>>& m, int n, vector<string>& ans,
-               vector<vector<int>>& vis, string move, int di[], int dj[]) {
-        if (i == n - 1 && j == n - 1) {
+    bool solve(int i, int j, vector<vector<int>> &m, int n, vector<string> &ans,
+               vector<vector<int>> &vis, string move, int di[], int dj[])
+    {
+        if (i == n - 1 && j == n - 1)
+        {
             ans.push_back(move);
             return true;
         }
         string dir = "DLRU";
-        for (int idx = 0; idx < 4; idx++) {
+        for (int idx = 0; idx < 4; idx++)
+        {
             int nexti = i + di[idx];
             int nextj = j + dj[idx];
             if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n &&
-                !vis[nexti][nextj] && m[nexti][nextj] == 1) {
+                !vis[nexti][nextj] && m[nexti][nextj] == 1)
+            {
                 vis[i][j] = 1;
                 if (solve(nexti, nextj, m, n, ans, vis, move + dir[idx], di, dj))
                     return true;
@@ -24,35 +29,42 @@ public:
         return false;
     }
 
-    vector<string> findPath(vector<vector<int>>& m, int n) {
+    vector<string> findPath(vector<vector<int>> &m, int n)
+    {
         vector<string> ans;
         vector<vector<int>> vis(n, vector<int>(n, 0));
-        int di[] = { +1, 0, 0, -1 };
-        int dj[] = { 0, -1, +1, 0 };
-        if (m[0][0] == 1) {
+        int di[] = {+1, 0, 0, -1};
+        int dj[] = {0, -1, +1, 0};
+        if (m[0][0] == 1)
+        {
             if (solve(0, 0, m, n, ans, vis, "", di, dj))
                 return ans;
         }
-        return { "-1" };
+        return {"-1"};
     }
 };
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
         vector<vector<int>> m(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 cin >> m[i][j];
             }
         }
         Solution obj;
         vector<string> result = obj.findPath(m, n);
         sort(result.begin(), result.end());
-        for (const auto& path : result) {
+        for (const auto &path : result)
+        {
             cout << path << " ";
         }
         cout << endl;
