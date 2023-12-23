d95fa1def15d487840c9bdf22b9b8b8c3a691a6d
Shreyash <shreyashmohadikar@gmail.com>
2023-12-23 17:14:17 +0530

    Buy Two Chocolates

---
+++ Practice/Buy Two Chocolates.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int left_money = money - (prices[0] + prices[1]);
+        if(left_money >= 0){
+            return left_money;
+        }
+        return money;
+    }
+};
