7afc1cecafb624ab8098359315caa3a4bf0f35aa
Shreyash <shreyashmohadikar@gmail.com>
2024-02-16 12:50:27 +0530

    Least Number of Unique Integers after K Removals

--- Practice/Least Number of Unique Integers after K Removals.cpp
+++ Practice/Least Number of Unique Integers after K Removals.cpp
@@ -1,26 +1,34 @@
-class Solution {
+class Solution
+{
 public:
-    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    int findLeastNumOfUniqueInts(vector<int> &arr, int k)
+    {
         unordered_map<int, int> mp;
-        for(int &it : arr){
+        for (int &it : arr)
+        {
             mp[it]++;
         }
-        priority_queue<pair<int, int>, 
-                        vector<pair<int, int>>, 
-                        greater<pair<int, int>>> pq;
-        for(auto &it : mp){
+        priority_queue<pair<int, int>,
+                       vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        for (auto &it : mp)
+        {
             pq.push({it.second, it.first});
         }
-        while(k){
+        while (k)
+        {
             auto temp = pq.top();
             pq.pop();
             int count = temp.first;
             int num = temp.second;
             k -= count;
-            if(k >= 0){
+            if (k >= 0)
+            {
                 continue;
             }
-            else{
+            else
+            {
                 pq.push({count, num});
                 break;
             }
